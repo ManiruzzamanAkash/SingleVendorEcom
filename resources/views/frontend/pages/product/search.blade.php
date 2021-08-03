3e35c1f1e6e6e4fc5ef6cb3655caac5a10bb9c34
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-08-03 23:36:49 +0600

    Fixed Frontend Search page and product list page

--- resources/views/frontend/pages/product/search.blade.php
+++ resources/views/frontend/pages/product/search.blade.php
@@ -1,46 +1,54 @@
 @extends('frontend.layouts.master')
 @section('stylesheets')
-    <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/app/search.css') }}?v1.0.2">
+<link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/app/search.css') }}?v1.0.2">
 @endsection
 @section('content')
 
-  <!-- Start Sidebar + Content -->
-  <div class='container content-holder'>
-
-
-       @if ($products->count() > 0)
-        <div class="titleArea">
-          Showing search results for "{{ $search }}"
-          <h6 class="text-success">You can also search from Category</h6>
-        </div>
-
-       
-          @include('frontend.pages.product.partials.all_products')
-        @else
-            
-<div class="empty-holder">
-  <div class="notFound">
-    Not found
-  </div>
-    <div class="emptyContent">
-        <div class="empty-logo">
-            <i class="fa fa-search" aria-hidden="true"></i>
-        </div>
-        <div class="empty-heading">
-            <!-- looks like you have no items in your shopping cart. -->
-            Oops there is no search results for "{{ $search }}" 
-        </div>
-        <div class="add-msg">
-            <h6 class="text-success">You can also search from Category</h6>
+<!-- Start Sidebar + Content -->
+<div class='container content-holder'>
+    <div class="titleArea">
+        <div>
+            <h6 class="text-success pull-left">
+                Showing search results for "{{ $search }}"
+                <br />
+                You can also search from Category
+            </h6>
+            <h6 class="pull-right">
+                @php
+                    $offers = [0, 10, 20, 30, 40, 50, 70, 80, 100];
+                @endphp
+                Offers: 
+                @foreach ($offers as $offer)
+                    <a class="btn btn-sm btn{{ request()->offer == $offer  ? '' : '-outline'}}-info" href="{{ route('search') }}?search={{ request()->search }}&offer={{ $offer }}">{{ $offer }}%</a>
+                @endforeach
+            </h6>
+            <div class="clearfix"></div>
         </div>
     </div>
+    
+    @if ($products->count() > 0)
+    @include('frontend.pages.product.partials.all_products')
+    @else
+
+    <div class="empty-holder">
+        <div class="notFound">
+            Not found
+        </div>
+        <div class="emptyContent">
+            <div class="empty-logo">
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+            <div class="empty-heading">
+                <!-- looks like you have no items in your shopping cart. -->
+                Oops there is no search results for "{{ $search }}"
+            </div>
+            <div class="add-msg">
+                <h6 class="text-success">You can also search from Category</h6>
+            </div>
+        </div>
+    </div>
+    @endif
 </div>
-        
-        @endif
 
-
-
-  </div>
-
-  <!-- End Sidebar + Content -->
+<!-- End Sidebar + Content -->
 @endsection
