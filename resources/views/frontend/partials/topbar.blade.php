deb52d3e8f713c6f708018b59afbd6ca7c12b227
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-08-04 01:59:06 +0600

    Moved some menus to top

--- resources/views/frontend/partials/topbar.blade.php
+++ resources/views/frontend/partials/topbar.blade.php
@@ -1,5 +1,20 @@
-<nav class=" bg-dark sticky-top ">
+<nav class="bg-dark sticky-top">
     <div class="container text-center text-light">
-        Extra 50% off Sale, prices as marked | Free shipping on orders of $49+
+        <div class="pull-left">
+            <a href="{{ route('search') }}?search=&offer=50" style="font-size: 12px; color: #fff">
+                Extra 50% off Sale, prices as marked | Get the sale right now !!
+            </a>
+        </div>
+        <div class="pull-right">
+            @if (!Auth::check())
+                <a href="{{ route('login') }}" class="text-white mr-2" style="font-size: 12px; color: #fff">
+                    Sign In
+                </a>
+                <a href="{{ route('register') }}" class="text-white mr-2" style="font-size: 12px; color: #fff">
+                    Sign Up
+                </a>
+            @endif
+        </div>
+        <div class="clearfix"></div>
     </div> 
 </nav>
