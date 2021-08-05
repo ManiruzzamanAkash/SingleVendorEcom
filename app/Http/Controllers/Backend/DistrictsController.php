c8798ae10fbd9129d0bedabb9ad811dd93020a2d
mrmoon007 <mrmoon025@gmail.com>
2021-08-05 13:11:41 +0600

    districts list done

--- app/Http/Controllers/Backend/DistrictsController.php
+++ app/Http/Controllers/Backend/DistrictsController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 
 use App\Models\District;
 use App\Models\Division;
+use DataTables;
 
 class DistrictsController extends Controller
 {
@@ -17,8 +18,54 @@ class DistrictsController extends Controller
   
   public function index()
   {
-    $districts = District::orderBy('name', 'asc')->get();
-    return view('backend.pages.districts.index', compact('districts'));
+    if (request()->ajax()) {
+
+      $districts = District::orderBy('id', 'asc')->get();
+
+      $datatable = DataTables::of($districts)
+        ->addIndexColumn()
+        ->addColumn(
+          'action',
+          function ($row) {
+            $csrf = "" . csrf_field() . "";
+            $method_delete = "" . method_field("post") . "";
+            $html = '';
+
+            $html .= '<a class="btn waves-effect waves-light btn-success btn-sm btn-circle ml-1 p-1 " title="Edit Blog Details" href="' . route('admin.district.edit', $row->id) . '"><i class="fa fa-edit"></i></a>';
+
+            $html .= '<a class="btn waves-effect waves-light btn-danger btn-sm btn-circle ml-1 p-1 text-white" title="Delete Admin" id="deleteItem' . $row->id . '"><i class="fa fa-trash"></i></a>';
+
+            $html .= '<script>
+                                    $("#deleteItem' . $row->id . '").click(function(){
+                                        swal.fire({ title: "Are you sure?",text: "Advertisement will be deleted as trashed !",type: "warning",showCancelButton: true,confirmButtonColor: "#DD6B55",confirmButtonText: "Yes, delete it!"
+                                        }).then((result) => { if (result.value) {$("#deletePermanentForm' . $row->id . '").submit();}})
+                                    });
+                                </script>';
+
+            $deleteRoute =  route('admin.district.delete', $row->id);
+            $html .= '
+                  <form id="deletePermanentForm' . $row->id . '" action="' . $deleteRoute . '" method="post" style="display:none">' . $csrf . $method_delete . '
+                      <button type="submit" class="btn waves-effect waves-light btn-rounded btn-success"><i
+                              class="fa fa-check"></i> Confirm Permanent Delete</button>
+                      <button type="button" class="btn waves-effect waves-light btn-rounded btn-secondary" data-dismiss="modal"><i
+                              class="fa fa-times"></i> Cancel</button>
+                  </form>';
+
+            return $html;
+          }
+        )
+
+        ->editColumn('division_id', function ($row) {
+          // $productImage = ProductImage::where('product_id', $row->id)->first();
+          return $row->division->name;
+        });
+        
+        $rawColumns = ['action','title', 'division_id'];
+        return $datatable->rawColumns($rawColumns)
+          ->make(true);
+    }
+    //$districts = District::orderBy('name', 'asc')->get();
+    return view('backend.pages.districts.index');
   }
 
   public function create()
--- resources/views/backend/pages/districts/index.blade.php
+++ resources/views/backend/pages/districts/index.blade.php
@@ -10,6 +10,60 @@
         </div>
         <div class="card-body">
             @include('backend.partials.messages')
+          <table class="table table-hover table-striped"  id="district_Table">
+            <thead>
+              <tr>
+                <th>#SL</th>
+                <th>District Name</th>
+                <th>Division Name</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+
+          </table>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <!-- main-panel ends -->
+@endsection
+
+@section('scripts')
+
+<script>
+  const ajaxURL = "<?php echo Route::is('admin/districts') ?>";
+  $('table#district_Table').DataTable({
+      dom: 'Blfrtip',
+      language: {processing: "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span> Loading Data..."},
+      processing: true,
+      serverSide: true,
+      ajax: {url: ajaxURL},
+      aLengthMenu: [[25, 50, 100, 1000, -1], [25, 50, 100, 1000, "All"]],
+      buttons: ['copy', 'csv','excel', 'pdf', 'print'],
+      columns: [
+          {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+          {data: 'name', name: 'name'},
+          {data: 'division_id', name: 'division_id'},
+          {data: 'action', name: 'action'}
+      ]
+  });
+</script>
+
+@endsection
+
+{{-- @extends('backend.layouts.master')
+
+@section('content')
+  <div class="main-panel">
+    <div class="content-wrapper">
+
+      <div class="card">
+        <div class="card-header">
+          Manage Districts
+        </div>
+        <div class="card-body">
+            @include('backend.partials.messages')
           <table class="table table-hover table-striped">
             <tr>
               <th>#</th>
@@ -64,4 +118,4 @@
     </div>
   </div>
   <!-- main-panel ends -->
-@endsection
+@endsection --}}
